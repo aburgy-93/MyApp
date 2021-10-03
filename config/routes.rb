5a4d1fd16119ac591a87ef9f903ec4e27774a0e7
Alex Burgy <burgyburger@gmail.com>
2021-10-03 12:06:41 -0400

    replaced roots routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-    resources :articles 
-    
+resources :posts
+resources :articles
     root 'welcome#index'
+    root 'welcome#articles'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
